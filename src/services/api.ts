e50d3219c0f27fd5e164978ab7e4bd78f2ea8c26
Carlos Defelicibus Junior <carlitosceo@gmail.com>
2025-09-08 16:28:32 -0300

    user-utm

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://api.insiderhof.com.br'
+  baseURL: 'https://api.insiderhof.com.br' 
   //baseURL: 'http://localhost:3007',
 })
 
