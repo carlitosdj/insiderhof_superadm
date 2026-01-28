6ceafc23f6a946ad06a0d4e190791c01fd38e64f
Carlos Defelicibus Junior <carlitosceo@gmail.com>
2026-01-28 18:02:08 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import commonjs from 'vite-plugin-commonjs'
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react(), commonjs()],
-  //base: "/metronic8/react/demo1/",
+  plugins: [react()],
   build: {
     chunkSizeWarningLimit: 3000,
   },
-  server:{
+  server: {
     port: 3003
   }
 })
