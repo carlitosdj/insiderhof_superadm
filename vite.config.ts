5cfd85122561d1bdab9a38415f293c7fb6149f14
Carlos Defelicibus Junior <carlitosceo@gmail.com>
2026-01-28 18:10:49 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,25 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+
   build: {
     chunkSizeWarningLimit: 3000,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      requireReturnsDefault: 'auto'
+    }
   },
+
+  optimizeDeps: {
+    include: [
+      'redux-saga',
+      'moment-duration-format',
+      'jspdf-autotable',
+      'xlsx',
+      'socket.io-client'
+    ]
+  },
+
   server: {
     port: 3003
   }
